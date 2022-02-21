2e945b3b2c7f6fc78cc26e4ebc8d44aed674efa0
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-02-21 23:06:41 +0300

    Basket.API

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{name}", Name = "Get")]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> Get(string name)
+        {
+            return Ok(await _repository.Get(name) ?? new ShoppingCart(name));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> Update([FromBody] ShoppingCart moedl)
+        {
+            return Ok(await _repository.Update(moedl));
+        }
+
+        [HttpDelete("{name}",Name = "Delete")]
+        [ProducesResponseType(typeof(void),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult> Delete(string name)
+        {
+            await _repository.Delete(name);
+            return Ok();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        public async Task Delete(string name)
+        {
+            await _cache.RemoveAsync(name);
+        }
+
+        public async Task<ShoppingCart> Get(string name)
+        {
+            var model = await _cache.GetStringAsync(name);
+            if (model == null)
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(model);
+        }
+
+        public async Task<ShoppingCart> Update(ShoppingCart model)
+        {
+            await _cache.SetStringAsync(model.UserName, JsonConvert.SerializeObject(model));
+            return await Get(model.UserName);
+        }
+    }
+}
