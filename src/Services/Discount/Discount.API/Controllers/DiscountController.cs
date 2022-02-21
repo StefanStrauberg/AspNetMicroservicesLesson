c1426b9ed7ad4c14adc67213fa4adb1b1eee21e1
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-02-22 01:19:49 +0300

    Discount.API

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,48 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{name}", Name = "Get")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> Get(string name)
+        {
+            return Ok(await _repository.Get(name));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> Post([FromBody] Coupon coupon)
+        {
+            await _repository.Create(coupon);
+            return CreatedAtRoute("Get", new {name = coupon.ProductName}, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> Update([FromBody] Coupon coupon)
+        {
+            return Ok(await _repository.Update(coupon));
+        }
+
+        [HttpDelete("{name}", Name = "Delete")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> Delete(string name)
+        {
+            return Ok(await _repository.Delete(name));
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> Get(string name);
+        Task<bool> Create(Coupon coupon);
+        Task<bool> Update(Coupon coupon);
+        Task<bool> Delete(string name);
+    }
+}
