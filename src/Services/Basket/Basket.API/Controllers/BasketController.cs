afe6cf81b0844fa6631a4ec7e605511dbd595d83
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-02-24 22:46:57 +0300

    Grpc communications

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -11,9 +12,11 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-        public BasketController(IBasketRepository repository)
+        private readonly DiscountGrpcService _discountGrpcService;
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository;
+            _discountGrpcService = discountGrpcService;
         }
 
         [HttpGet("{name}", Name = "Get")]
@@ -25,9 +28,14 @@ namespace Basket.API.Controllers
 
         [HttpPost]
         [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
-        public async Task<ActionResult<ShoppingCart>> Update([FromBody] ShoppingCart moedl)
+        public async Task<ActionResult<ShoppingCart>> Update([FromBody] ShoppingCart model)
         {
-            return Ok(await _repository.Update(moedl));
+            foreach (var item in model.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+            return Ok(await _repository.Update(model));
         }
 
         [HttpDelete("{name}",Name = "Delete")]
