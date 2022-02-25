4cd2ec094b118bae7c51d423f7e9fa4402b7f6e1
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-02-26 00:22:06 +0300

    Ordering.API

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<Trequest, Tresponse> : IPipelineBehavior<Trequest, Tresponse>
+    {
+        private readonly IEnumerable<IValidator<Trequest>> _validators;
+        public ValidationBehaviour(IEnumerable<IValidator<Trequest>> validators)
+        {
+            _validators = validators;
+        }
+        public Task<Tresponse> Handle(Trequest request, CancellationToken cancellationToken, RequestHandlerDelegate<Tresponse> next)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+        private readonly IMapper _mapper;
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToDelete == null)
+            {
+                throw new NotFoundException(nameof(request),request.Id);
+            }
+            await _orderRepository.DeleteAsync(orderToDelete);
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+            return Unit.Value;
+        }
+    }
+}
